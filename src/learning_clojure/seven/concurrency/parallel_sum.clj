2272dbc25885685c690319513c3de8752c5aad0f
Juan Reséndiz <jresendiz27@gmail.com>
2018-09-21 08:16:45 -0500

    Add simple sum (using reduce) and word-frequencies function example

--- src/learning_clojure/seven/concurrency/parallel_sum.clj
+++ src/learning_clojure/seven/concurrency/parallel_sum.clj
@@ -1,5 +1,13 @@
 (ns learning-clojure.seven.concurrency.parallel-sum
   (:require [clojure.core.reducers :as red]))
 
+(defn simple-sum [numbers]
+  (reduce + numbers))
+
 (defn parallel-sum [numbers]
   (red/fold + numbers))
+
+(defn word-frequencies [words]
+  (reduce
+    (fn [counts word] (assoc counts word (inc (get counts word 0))))
+    {} words))
