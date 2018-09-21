b734e76db207d00dd4a4362220c5064d5972c252
Juan Reséndiz <jresendiz27@gmail.com>
2018-09-21 08:16:18 -0500

    Update test with large range of numbers

--- test/learning_clojure/seven/concurrency/parallel_sum_test.clj
+++ test/learning_clojure/seven/concurrency/parallel_sum_test.clj
@@ -1,9 +1,15 @@
 (ns learning-clojure.seven.concurrency.parallel-sum-test
   (:require [clojure.test :refer :all])
-  (:use learning-clojure.seven.concurrency.parallel-sum))
+  (:use [learning-clojure.seven.concurrency.parallel-sum :only [parallel-sum simple-sum]]))
 
 (def numbers [1, 2, 3, 4, 5, 6, 7, 8])
 
+(def large-range (into [] (range 0 1000000)) )
+
+(deftest compare-sums
+  (testing "Compare the sums"
+    (is (<  (time (parallel-sum large-range))
+            (time (simple-sum large-range))))))
 (deftest parallel-sum-test
   (testing "Parallel-sum using reducers and clojure core"
     (is (= (parallel-sum numbers) 36))))
